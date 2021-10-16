90931e84a2e07f564478f38672e3b1f7224aa97e
wzdev-ci <61424532+wzdev-ci@users.noreply.github.com>
2021-10-16 21:52:25 +0000

    Generate site: Sat Oct 16 21:52:25 UTC 2021

--- scripts/common_objects.js
+++ scripts/common_objects.js
@@ -64,7 +64,7 @@ var PropulsionTypeToSpeedFactorMap = {
     "Half-Tracked": "half-tracked",
 };
 
-var current_site_version = "4.2.0-beta1-v3";
+var current_site_version = "4.2.0-beta2";
 
 $(function () {
 
