bc6ab4957708379f95b13bc0cc4a92ac6a84c2a9
KJeff01 <kjeff1997@gmail.com>
2023-11-06 19:02:39 -0600

    Update for 4.4.0

--- wz2100-database-project/scripts/common_objects.js
+++ wz2100-database-project/scripts/common_objects.js
@@ -64,7 +64,7 @@ var PropulsionTypeToSpeedFactorMap = {
     "Half-Tracked": "half-tracked",
 };
 
-var current_site_version = "4.4.0-beta1";
+var current_site_version = "4.4.0";
 
 $(function () {
 
