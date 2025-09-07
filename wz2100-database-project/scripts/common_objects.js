b39df63e4b80c02c9c63b7dce105346a5cfd2b60
KJeff01 <kjeff1997@gmail.com>
2025-09-07 18:37:44 -0500

    Fix a typo

--- wz2100-database-project/scripts/common_objects.js
+++ wz2100-database-project/scripts/common_objects.js
@@ -64,7 +64,7 @@ var PropulsionTypeToSpeedFactorMap = {
     "Half-Tracked": "half-tracked",
 };
 
-var current_site_version = "4.6.0-revision1";
+var current_site_version = "4.6.0-revision2";
 
 $(function () {
 
